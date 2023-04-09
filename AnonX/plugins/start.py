4b7f9def234b248c6a33527e35cc8cf1127bb315
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2023-04-09 08:09:52 +0700

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -16,7 +16,7 @@ from youtubesearchpython.__future__ import VideosSearch
 
 import config
 from config import BANNED_USERS
-from config.config import OWNER_ID
+from config.import OWNER_ID
 from strings import get_command, get_string
 from AnonX import Telegram, YouTube, app
 from AnonX.misc import SUDOERS
