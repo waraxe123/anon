b998cbca038938da1bbdb21587ba6874c4fd4809
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2023-04-11 18:51:46 +0700

    Update callback.py

--- AnonX/plugins/callback.py
+++ AnonX/plugins/callback.py
@@ -46,14 +46,12 @@ async def del_back_playlist(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     callback_request = callback_data.split(None, 1)[1]
     videoid, chat_id = callback_request.split("|")
-    chat_id = CallbackQuery.message.chat.id
-
-    buttons = panel_markup_1(_, videoid, chat_id)
     if videoid == str(None):
         buttons = telegram_markup(_, chat_id)
     else:
         buttons = stream_markup(_, videoid, chat_id)
-    chat_id = CallbackQuery.message.chat.id
+    chat_id = CallbackQuery.message.chat.id 
+    buttons = panel_markup_1(_, videoid, chat_id)
     try:
         await CallbackQuery.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup(buttons)
