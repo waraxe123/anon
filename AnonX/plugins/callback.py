30b4c2f6db6949bed93449b77480f0202e940664
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2023-04-12 06:31:55 +0700

    Update callback.py

--- AnonX/plugins/callback.py
+++ AnonX/plugins/callback.py
@@ -34,8 +34,10 @@ from AnonX.utils.inline import (
     close_keyboard,
 )
 from AnonX.utils.inline.play import (panel_markup_1, 
-                                          stream_markup, 
-                                          telegram_markup)
+                                     panel_markup_2,
+                                     panel_markup_3,
+                                     stream_markup, 
+                                     telegram_markup)
 from AnonX.utils.stream.autoclear import auto_clean
 from AnonX.utils.thumbnails import gen_thumb
 
