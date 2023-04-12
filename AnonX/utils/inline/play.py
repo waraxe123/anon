b7cf31e65ed37cb4024417bb6af9868089459336
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2023-04-12 16:42:07 +0700

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -163,24 +163,7 @@ def telegram_markup(_, videoid, chat_id):
     buttons = [
         [
             
-            InlineKeyboardButton(
-                text="▷",
-                callback_data=f"ADMIN Resume|{chat_id}"
-            ),
-            InlineKeyboardButton(
-                text="II", callback_data=f"ADMIN Pause|{chat_id}" 
-            ),
-            InlineKeyboardButton(
-                text=_["PL_B_2"], callback_data=f"add_playlist {videoid}"
-            ),
-            InlineKeyboardButton(
-                text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
-            ),
-            InlineKeyboardButton(
-                text="▢", callback_data=f"ADMIN Stop|{chat_id}"
-            ),
-        ],
-        [
+            
             InlineKeyboardButton(
                 text=_["PL_B_3"],
                 callback_data=f"PanelMarkup {videoid}|{chat_id}"
