c9b921e4aebede08c03bed3fff0e6639e6f87936
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2023-04-12 20:42:14 +0700

    Update callback.py

--- AnonX/plugins/callback.py
+++ AnonX/plugins/callback.py
@@ -50,7 +50,8 @@ async def markup_panel(client, CallbackQuery: CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     callback_request = callback_data.split(None, 1)[1]
     videoid, chat_id = callback_request.split("|")
-    
+    chat_id = CallbackQuery.message.chat.id
+    buttons = panel_markup_1(_, videoid, chat_id)
     try:
         await CallbackQuery.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup(buttons)
@@ -122,11 +123,15 @@ async def del_back_playlist(client, CallbackQuery, _):
         if pages == 0:
             buttons = panel_markup_2(_, videoid, chat_id)
         if pages == 2:
+            buttons = panel_markup_1(_, videoid, chat_id)
+        if pages == 1:
             buttons = panel_markup_3(_, videoid, chat_id)
     if state == "Back":
         if pages == 2:
             buttons = panel_markup_2(_, videoid, chat_id)
         if pages == 1:
+            buttons = panel_markup_1(_, videoid, chat_id)
+        if pages == 0:
             buttons = panel_markup_3(_, videoid, chat_id)
     try:
         await CallbackQuery.edit_message_reply_markup(
