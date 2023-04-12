3e6f431a9762ae620138bdd4a89645e61c942f8e
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2023-04-13 02:49:52 +0700

    Update memorydatabase.py

--- AnonX/utils/database/memorydatabase.py
+++ AnonX/utils/database/memorydatabase.py
@@ -21,7 +21,7 @@ langdb = mongodb.language
 authdb = mongodb.adminauth
 videodb = mongodb.yukkivideocalls
 onoffdb = mongodb.onoffper
-suggdb = mongodb.suggestion
+
 autoenddb = mongodb.autoend
 
 
@@ -42,7 +42,7 @@ cleanmode = []
 nonadmin = {}
 vlimit = []
 maintenance = []
-suggestion = {}
+
 autoend = {}
 
 
@@ -81,30 +81,10 @@ async def autoend_off():
 # SUGGESTION
 
 
-async def is_suggestion(chat_id: int) -> bool:
-    mode = suggestion.get(chat_id)
-    if not mode:
-        user = await suggdb.find_one({"chat_id": chat_id})
-        if not user:
-            suggestion[chat_id] = True
-            return True
-        suggestion[chat_id] = False
-        return False
-    return mode
 
 
-async def suggestion_on(chat_id: int):
-    suggestion[chat_id] = True
-    user = await suggdb.find_one({"chat_id": chat_id})
-    if user:
-        return await suggdb.delete_one({"chat_id": chat_id})
 
 
-async def suggestion_off(chat_id: int):
-    suggestion[chat_id] = False
-    user = await suggdb.find_one({"chat_id": chat_id})
-    if not user:
-        return await suggdb.insert_one({"chat_id": chat_id})
 
 
 # LOOP PLAY
