923ca281255dce7b5d03bb69fb6ab4399a050820
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2023-04-13 04:47:07 +0700

    Update callback.py

--- AnonX/plugins/callback.py
+++ AnonX/plugins/callback.py
@@ -30,7 +30,7 @@ from YukkiMusic.utils.inline.play import (panel_markup_1,
                                           panel_markup_2,
                                           panel_markup_3,
                                           stream_markup,
-                                          telegram_markup) hi
+                                          telegram_markup) 
 from AnonX.utils.inline import (
     stream_markup,
     stream_markup_timer,
